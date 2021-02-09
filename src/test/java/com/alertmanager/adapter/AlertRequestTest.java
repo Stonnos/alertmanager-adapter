8f3ddc5be62eb539330466fa3878e90a8af48286
Батыгин Роман Игоревич <batygin@self.team>
2021-02-09 17:57:28 +0700

    #ES-69 - added java doc to AlertRequestTest

--- src/test/java/com/alertmanager/adapter/AlertRequestTest.java
+++ src/test/java/com/alertmanager/adapter/AlertRequestTest.java
@@ -10,13 +10,20 @@ import java.io.InputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * Unit test for mapping alert request from json to java class model.
+ *
+ * @author Roman Batygin
+ */
 class AlertRequestTest {
+
+    private static final String ALERT_JSON = "alert.json";
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     void testMapAlertRequest() throws IOException {
-        @Cleanup InputStream inputStream = AlertRequestTest.class.getClassLoader().getResourceAsStream("alert.json");
+        @Cleanup InputStream inputStream = AlertRequestTest.class.getClassLoader().getResourceAsStream(ALERT_JSON);
         AlertRequest alertRequest = objectMapper.readValue(inputStream, AlertRequest.class);
         assertThat(alertRequest).isNotNull();
     }
