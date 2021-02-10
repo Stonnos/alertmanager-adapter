531e1ffabea27425514f4e8348351ab3a95a1a67
Батыгин Роман Игоревич <batygin@self.team>
2021-02-10 19:58:58 +0700

    #ES-74 - some tests

--- src/test/java/com/alertmanager/adapter/service/template/impl/FreemarkerTemplateProcessorServiceTest.java
+++ src/test/java/com/alertmanager/adapter/service/template/impl/FreemarkerTemplateProcessorServiceTest.java
@@ -2,6 +2,7 @@ package com.alertmanager.adapter.service.template.impl;
 
 import com.alertmanager.adapter.exception.TemplateProcessingException;
 import freemarker.template.Configuration;
+import freemarker.template.Template;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,7 +12,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.io.IOException;
 import java.util.Collections;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 /**
@@ -34,5 +37,13 @@ class FreemarkerTemplateProcessorServiceTest {
         when(configuration.getTemplate(TEST_TEMPLATE)).thenThrow(new IOException());
         assertThrows(TemplateProcessingException.class, () -> templateProcessorService.process(TEST_TEMPLATE,
                 Collections.emptyMap()));
+    }
+
+    @Test
+    void testProcessTemplate() throws IOException {
+        Template template = mock(Template.class);
+        when(configuration.getTemplate(TEST_TEMPLATE)).thenReturn(template);
+        String message = templateProcessorService.process(TEST_TEMPLATE, Collections.emptyMap());
+        assertThat(message).isNotNull();
     }
 }
